c8c7dfbd4870b70f7312969eca547968dc738d51
SurveshRathore <educatedevilboy@gmail.com>
2022-10-20 22:44:48 +0530

    stack class

---
+++ Stack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14StackAndQueue
+{
+    class Stack
+    {
+        public int data;
+        public Stack next;
+        public Stack(int data)
+        {
+            this.data = data;
+        }
+    }
+}
