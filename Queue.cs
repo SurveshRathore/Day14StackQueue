f5e1afb1bfe4f905c464d8a4a9a9210046068bb7
SurveshRathore <educatedevilboy@gmail.com>
2022-10-20 22:45:14 +0530

    Queue class

---
+++ Queue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14StackAndQueue
+{
+    class Queue
+    {
+        public int data;
+        public Queue next;
+        public Queue(int data)
+        {
+            this.data = data;
+        }
+    }
+}
